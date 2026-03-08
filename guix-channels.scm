a60e8e49b3a5cd631e5fdd76bb87319e801a0792
Jack Faller <jack.t.faller@gmail.com>
2026-03-08 06:17:43 +0000

    Add guix-channels.scm

---
+++ guix-channels.scm
@@ -0,0 +1,11 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.guix.gnu.org/guix.git")
+        (branch "master")
+        (commit
+          "31ba754c15ebb80d859e3ad4df233db4cda98e6a")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
