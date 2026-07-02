9fdd6aa9027962de6428f03641f9e10a605b1ba0
Jack Faller <jack.t.faller@gmail.com>
2026-07-02 21:28:52 +0100

    Removes error

--- guix/xyz/jackfaller/miny.scm
+++ guix/xyz/jackfaller/miny.scm
@@ -56,7 +56,6 @@
                    (delete 'configure)
 		   (replace 'install
 		     (lambda* (#:key outputs #:allow-other-keys)
-               (error "a")
 		       (let* ((out (assoc-ref outputs "out"))
 			      (bin (string-append out "/bin")))
 			 (install-file "miny" bin)))))
